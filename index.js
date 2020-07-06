a11d5df8942b53e495f7cbce1035844edc0b3131
gloryawuy <S21610521@student.unklab.ac.id>
2020-07-06 14:45:54 +0800

    edited index.html, style.css dan menambahkan index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var myCollection = document.getElementsByTagName("p");
+    var i;
+    for (i = 0; i < myCollection.length; i++) {
+      myCollection[i].style.color = "Black";
+        myCollection[i].style.background = "white";
+      }
+      function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+    }
+    function Experiment() {
+    document.getElementById("demo").innerHTML = "I am very Enthusiastic";
+  }
+  function sayHello() {
+    document.getElementById("demo1").innerHTML = "Thank You ";
+    document.getElementById("demo2").innerHTML = "I felt the atmosphere that I felt was a little different and a new lesson";
+    document.getElementById("demo3").innerHTML = "see you next time"
+    document.getElementById("demo4").innerHTML = "<--Kelas Web Design Summer 2020-->";
+  }
